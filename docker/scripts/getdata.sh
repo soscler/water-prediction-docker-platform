b5d61860c378faceb0635673f1900932f47cc2bc
Unknown <osler.santi@gmail.com>
2019-03-03 14:15:35 +0100

    add dockerfiles

--- docker/scripts/getdata.sh
+++ docker/scripts/getdata.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-export DATA_FOLDER_NAME=spark_ml
+export DATA_FOLDER_NAME=/spark_ml
 export DATA_FILE_NAME=AdultCensusIncome.csv
 export DATA_URL=https://amldockerdatasets.azureedge.net/AdultCensusIncome.csv
 
+mkdir -p ${DATA_FOLDER_NAME}
 echo "Downloading the data set..."
+
 if [ ! -f ${DATA_FOLDER_NAME}/${DATA_FILE_NAME} ]; then
-	wget -O ${DATA_FOLDER_NAME}/${DATA_FILE_NAME}  ${DATA_FILE_URL};
+	wget -O ${DATA_FOLDER_NAME}/${DATA_FILE_NAME}  ${DATA_URL};
 fi
 echo "Create the folder ${DATA_FOLDER_NAME} and put the data..."
-hdfs dfs -mkdir ${DATA_FOLDER_NAME} &&  hdfs dfs -put ${DATA_FILE_NAME} ${DATA_FOLDER_NAME}/${DATA_FILE_NAME}
+pwd
+hdfs dfs -mkdir -p ${DATA_FOLDER_NAME} &&  hdfs dfs -put -f ${DATA_FOLDER_NAME}/${DATA_FILE_NAME} ${DATA_FOLDER_NAME}/${DATA_FILE_NAME}
