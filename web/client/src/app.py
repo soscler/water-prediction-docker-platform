f18d6fd1da818404b6bb97805cd30e6754ea5b22
Jarvin-M <jarvinm19@gmail.com>
2019-03-11 22:46:17 +0100

    Update with cassandra

--- web/client/src/app.py
+++ web/client/src/app.py
@@ -16,31 +16,43 @@ session.execute("""
         WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '1' }
         """ % KEYSPACE)
 session.set_keyspace(KEYSPACE)
+# session.execute("""
+#         CREATE TABLE IF NOT EXISTS tempgallons (
+#             temp_id bigint,
+#             temp bigint,
+#             gallons bigint,
+#             PRIMARY KEY (temp_id)
+#         )
+#         """)
 session.execute("""
-        CREATE TABLE IF NOT EXISTS tempgallons (
-            temp_id bigint,
-            temp bigint,
-            gallons bigint,
-            PRIMARY KEY (temp_id)
+        CREATE TABLE IF NOT EXISTS testpredictions (
+            id bigint,
+            yyyymmdd timestamp,
+            hh bigint,
+            gallons float,
+            prediction float,
+            PRIMARY KEY (id,yyyymmdd)
         )
-        """)
-session.execute(""" INSERT INTO tempgallons (temp_id, temp ,gallons) VALUES(1,2,4) """)        
+        """)        
+# session.execute(""" INSERT INTO testpredictions (id, year, hour , gallons, predicted_gallons) VALUES(uuid(),20190308,05,1.45, 0.96) """)        
 
 
 @app.route("/")
 def post_to_front():
-    rows = session.execute('SELECT * FROM tempgallons')
+    rows = session.execute('SELECT * FROM testpredictions')
     pulled =[]
     for row in rows:
         print(row)
         each_row ={
-            'temp_id':row.temp_id,
-            'temp':row.temp,
-            'Gallons': row.gallons
+            'id':row.id,
+            'Time': row.hh,
+            'year':row.yyyymmdd,
+            'Gallons': row.gallons,
+            'predicted_gallons': row.prediction
         }
         pulled.append(each_row)
-    msg=jsonify({'result': pulled})
-    return render_template("index.html",msg=msg.response)
+    # msg=jsonify({'result': pulled})
+    return render_template("index.html",msg=pulled)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int("5000"))
