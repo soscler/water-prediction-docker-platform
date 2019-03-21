11f4ed14b78e2ea6dad9c1a128e9ee10f644d73b
anostdev <osler.santi@gmail.com>
2019-03-21 19:25:36 +0100

    kafka docker compose

--- web/server/src/main/java/com/scac/server/stream/utils/CsvReader.java
+++ web/server/src/main/java/com/scac/server/stream/utils/CsvReader.java
@@ -3,41 +3,38 @@ package com.scac.server.stream.utils;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.HashMap;
 
-
+@Component
 public class CsvReader implements Runnable{
+    @Value("${timer}")
+    private long timeInterval;
 
-    final long timeInterval;
+    private Reader reader;
 
-
+    @Value("${datafile}")
+    private  String file;
+    private HashMap<Headers, String> data = new HashMap<Headers, String>();
 
     public static enum Headers{
         ID,STN,YYYYMMDD,HH,DD,FH,FF,FX,T,T10,TD,SQ,Q,DR,RH,P,VV,N,U,WW,IX,M,R,S,O,Y
     }
-    private Reader reader;
-    private String file;
-    public static HashMap<Headers, String> data;
 
 
-    public CsvReader(String file, long timer){
-        this.file = file;
-        this.timeInterval = timer;
-        data = new HashMap<>();
-    }
+    // TODO: Read a file from an url
 
     public void read() throws IOException {
         System.out.println("About to read the csv file");
+        System.out.println(file);
+        // Lazy-loading
         reader = new FileReader(this.file);
-
-        // TODO: Read a file from an url
-
         Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(this.reader);
-
         for (CSVRecord record : records) {
             try {
 
@@ -51,7 +48,6 @@ public class CsvReader implements Runnable{
                 data.put(Headers.P , record.get(Headers.P));
                 data.put(Headers.VV , record.get(Headers.VV));
                 data.put(Headers.U , record.get(Headers.U));
-
                 Thread.sleep(timeInterval);
 
             } catch (InterruptedException e)
@@ -71,5 +67,26 @@ public class CsvReader implements Runnable{
         }
     }
 
+    public HashMap<Headers, String> getData() {
+        return data;
+    }
+
+    @Value("${timer}")
+    public void setTimeInterval(long timeInterval) {
+        this.timeInterval = timeInterval;
+    }
+
+    @Value("${datafile}")
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    public long getTimeInterval() {
+        return timeInterval;
+    }
+
+    public String getFile() {
+        return file;
+    }
 
 }
